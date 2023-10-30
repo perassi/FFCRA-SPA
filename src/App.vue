3d691e5f90c070437dd3a4e9a0ca1746054e9f40
perassi <horacio2jp@gmail.com>
2023-10-30 21:24:00 +0200

    taiwling setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LayOut>dsds</LayOut>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import LayOut from './components/LayOut.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    LayOut
   }
 }
 </script>
---
+++ src/components/LayOut.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <ul >
+    <li>FFCRA Information</li>
+    <li>FAQs</li>
+    <li>Profile</li>
+  </ul>
+  <section>
+    <slot></slot>
+  </section>
+  </div>
+</template>
+
+  <script>
+  export default {
+    name: 'LayOut',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
