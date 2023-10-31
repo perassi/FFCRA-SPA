74963bee791f667402a99ae795bbcfcda37c2c07
perassi <horacio2jp@gmail.com>
2023-10-31 23:07:01 +0200

    table progress

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <LayOut>dsds</LayOut>
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <LayOut>
+    <h2  class="text-[40px] underline text-[#3E435F]">Qualified Sick Leave</h2>
+    <p><span class="text-[#3E435F]">Self Leave</span> <span class="text-[#0047CC]">Reason for Qualified Sick Leave (may not exceed 10 workdays or 80 hours)</span></p>
+
+    <ul class="text-[16px] text-[#3E435F]">
+      <li class="leading-[35px] flex items-center gap-[10px]">
+        <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5.91416 8.1L4.51416 9.5L9.01416 14L19.0142 4L17.6142 2.6L9.01416 11.2L5.91416 8.1ZM18.0142 10C18.0142 14.4 14.4142 18 10.0142 18C5.61416 18 2.01416 14.4 2.01416 10C2.01416 5.6 5.61416 2 10.0142 2C10.8142 2 11.5142 2.1 12.2142 2.3L13.8142 0.7C12.6142 0.3 11.3142 0 10.0142 0C4.51416 0 0.0141602 4.5 0.0141602 10C0.0141602 15.5 4.51416 20 10.0142 20C15.5142 20 20.0142 15.5 20.0142 10H18.0142Z" fill="#3E435F"/>
+        </svg>
+        <span>Employee's own quarantine or isolation order under federal, state or local law</span>
+      </li>
+      <li class="leading-[35px] flex items-center gap-[10px]">
+        <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5.91416 8.1L4.51416 9.5L9.01416 14L19.0142 4L17.6142 2.6L9.01416 11.2L5.91416 8.1ZM18.0142 10C18.0142 14.4 14.4142 18 10.0142 18C5.61416 18 2.01416 14.4 2.01416 10C2.01416 5.6 5.61416 2 10.0142 2C10.8142 2 11.5142 2.1 12.2142 2.3L13.8142 0.7C12.6142 0.3 11.3142 0 10.0142 0C4.51416 0 0.0141602 4.5 0.0141602 10C0.0141602 15.5 4.51416 20 10.0142 20C15.5142 20 20.0142 15.5 20.0142 10H18.0142Z" fill="#3E435F"/>
+        </svg>
+        <span>Employee's self-quarantine as advised by a health care provider</span>
+      </li>
+      <li class="leading-[35px] flex items-center gap-[10px]">
+        <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5.91416 8.1L4.51416 9.5L9.01416 14L19.0142 4L17.6142 2.6L9.01416 11.2L5.91416 8.1ZM18.0142 10C18.0142 14.4 14.4142 18 10.0142 18C5.61416 18 2.01416 14.4 2.01416 10C2.01416 5.6 5.61416 2 10.0142 2C10.8142 2 11.5142 2.1 12.2142 2.3L13.8142 0.7C12.6142 0.3 11.3142 0 10.0142 0C4.51416 0 0.0141602 4.5 0.0141602 10C0.0141602 15.5 4.51416 20 10.0142 20C15.5142 20 20.0142 15.5 20.0142 10H18.0142Z" fill="#3E435F"/>
+        </svg>
+        <span>Employee is experiencing symptoms and seeking a medical diagnosis</span>
+      </li>
+    </ul>
+    <FormItem/>
+  </LayOut>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import LayOut from './components/LayOut.vue'
+import FormItem from './components/FormItem.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
-    LayOut
+    LayOut,
+    FormItem
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #F5F7FB;
+
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/FormItem.vue
@@ -0,0 +1,92 @@
+<template>
+<div>
+ 
+<div class="relative overflow-x-auto">
+    
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+  <div class="grid grid-cols-12 gap-4 bg-[#D6DDEB] px-[20px] py-[20px] text-[15px]">
+    <div>Employee Name</div>
+    <div>Regular Rate of Pay (hourly)</div>
+    <div class="col-span-2">Hours of qualified sick leave</div>
+    <div># of days qualified sick</div>
+    <div class="col-span-3">Dates employee was out	Employee </div>
+    <div class="col-span-1">Employee Total Sick Pay</div>
+    <div class="col-span-1">Reimbursement per employee</div>
+  </div>
+  <div class="grid grid-cols-12 gap-4 ">
+    <div>John Doe</div>
+    <div>$20.00</div>
+    <div class="col-span-2">80</div>
+    <div>10</div>
+    <div class="col-span-3">Dates employee was out	Employee </div>
+    <div class="col-span-1">$1,600.00</div>
+    <div class="col-span-1">$1,600.00</div>
+  </div>
+</div>
+
+</div>
+
+
+
+<form v-on:submit="didSubmitForm">
+  <div v-for="(item, index) in items" :key="index">
+    
+    <label :for="`data[${index}]name`">name:</label>
+    <input :name="`data[${index}]name`" required type="text" v-model="item.name">
+    
+    <label :for="`data[${index}]email`">email:</label>
+    <input :name="`data[${index}]email`" required type="email" v-model="item.email">
+    
+    <label :for="`data[${index}]enabled`">enabled:</label>
+    <input :name="`data[${index}]enabled`" type="checkbox" v-model="item.enabled">
+
+    <button v-on:click="items.splice(index, 1)" type="button">
+      &times;
+    </button>
+    
+    <hr>
+  </div>
+  
+  <button v-on:click="items.push({})" type="button">
+    add
+  </button>
+
+  <br>
+  
+  <button type="submit">save</button>
+</form>
+
+<pre>{{ items }}</pre>
+</div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'FormItem',
+    components: {},
+    data() {
+    return {
+      items: [
+        {
+          name: 'Test',
+          email: 'text@example.com',
+          enabled: true,
+        },
+      ],
+    };
+  },
+  methods: {
+   didSubmitForm(e) {
+     e.preventDefault();
+     
+     const data = new FormData(e.target);
+     
+     console.log('Form data:', data);
+   } 
+  },
+  }
+  </script>
+  
+  <style> </style>
+  
