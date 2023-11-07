aec42e832e347c3a847478fc726c4d58699421a8
perassi <horacio2jp@gmail.com>
2023-11-07 16:02:24 +0530

    cleanup

--- src/firebase.js
+++ src/firebase.js
@@ -1,34 +1,5 @@
-/*import {initializeApp} from 'firebase/app'
-import {getFirestore, collection} from 'firebase/firestore'
-
-export const firebaseApp = initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-})
-
-export const db = getFirestore(firebaseApp)
-export const ffcraRef = collection(db, 'ffcra-responses')*/
-
-
 import firebase from "firebase/app";
 import "firebase/firestore";
-
-console.log(process.env.FIREBASE_API_KEY)
-
-console.log(JSON.stringify({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-}))
 
 const firebaseApp = firebase.initializeApp({
     apiKey: 'AIzaSyCFNCayV_1c2OB_JITWO23sUL1kPHZrLTQ',
