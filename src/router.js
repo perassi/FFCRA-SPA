7cb5f6b4e220327246d153da440be50e4272aaa7
perassi <horacio2jp@gmail.com>
2023-11-07 16:58:37 +0530

    carry forward email param

--- src/router.js
+++ src/router.js
@@ -1,15 +1,22 @@
-import { createRouter } from 'vue-router'
+import {createRouter} from 'vue-router'
 
 const routes = [
-  { path: '/faqs', component: () => import('./pages/FAQs.vue') },
-  { path: '/profile', component: () => import('./pages/Profile.vue') },
-  { path: '/', component: () => import('./pages/FFCRAInformation.vue') },
+    {path: '/faqs', component: () => import('./pages/FAQs.vue')},
+    {path: '/profile', component: () => import('./pages/Profile.vue')},
+    {path: '/', component: () => import('./pages/FFCRAInformation.vue')},
 
 ]
 
-export default function(history) {
-    return createRouter({
-      history,
-      routes
+export default function (history) {
+    const router = createRouter({
+        history,
+        routes
     })
-  }
+    router.beforeEach((to, from) => {
+        const query = from.query
+        if (query.email)
+            to.query.email = query.email
+        return true
+    })
+    return router
+}
