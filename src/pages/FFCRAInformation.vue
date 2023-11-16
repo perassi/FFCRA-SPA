2c5f6b4ec42fbb24501be52e32f8f91802461aa3
perassi <horacio2jp@gmail.com>
2023-11-16 18:07:46 +0200

    need help ui

--- src/pages/FFCRAInformation.vue
+++ src/pages/FFCRAInformation.vue
@@ -28,6 +28,12 @@
                 :twoThousandLogic="true" :list="qualifiedFamilyLeave" @onDataChange="updateData"/>
     </section>
     <section class="order-1 2xl:order-2">
+      <p className="text-nowrap xl:flex sm:hidden gap-2 2xl:mt-[20px] 2xl:fixed font-[600] w-full items-center text-[18px] 2xl:w-[280px] justify-end cursor-pointer mb-[20px]"> <img v-bind:src="require('../assets/blue-support.svg')" /> <span>Need</span><span>help?</span>
+        <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.906211 6.31844C0.953923 6.32618 1.00221 6.32975 1.05052 6.32913L9.27667 6.32912L9.0973 6.41255C8.92197 6.49554 8.76246 6.60848 8.62592 6.74627L6.3191 9.05309C6.01529 9.34311 5.96424 9.80967 6.19813 10.1585C6.47034 10.5303 6.99238 10.611 7.36416 10.3388C7.39419 10.3168 7.42274 10.2928 7.44957 10.267L11.621 6.09552C11.947 5.76989 11.9473 5.24165 11.6217 4.91565C11.6215 4.91544 11.6212 4.91521 11.621 4.915L7.44957 0.743534C7.12331 0.418186 6.59507 0.418915 6.2697 0.745176C6.24409 0.770856 6.22019 0.798179 6.19813 0.826962C5.96424 1.17583 6.01529 1.64238 6.3191 1.9324L8.62175 4.24339C8.74416 4.36593 8.88489 4.46868 9.0389 4.54791L9.28919 4.66054L1.09643 4.66054C0.670237 4.64471 0.296317 4.94243 0.216251 5.36135C0.142495 5.81617 0.451392 6.24465 0.906211 6.31844Z" fill="#3E435F"/>
+        </svg>
+        </p>
+
       <ul class="bg-[#FFFFFF] border-[#0047CC] border-[2px] rounded-[10px] min-w-[280px] pt-[10px] 2xl:mt-[117px] 2xl:fixed">
         <li class="text-[21px] font-[600] text-center mb-[21px] xl:max-w-[174px] m-auto">Money Back to the company</li>
         <li class="text-[18px] flex justify-between bg-[#F5F7FB] mx-[11px] px-[19px] xl:px-[8px]"><span
